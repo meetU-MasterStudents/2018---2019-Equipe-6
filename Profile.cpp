a141aa438e18ffcbcb8c9c63f965dbbd13f1826c
Benedicte Colnet <benedicte.colnet@gmail.com>
2018-11-02 20:27:00 +0100

    Read Homstrad first try

--- Profile.cpp
+++ Profile.cpp
@@ -1,4 +1,17 @@
 #include "Profile.h"
+#include <stdlib.h>
+#include <stdio.h>
+/* stdlib pour exit(), et stdio pour puts() */
+#include <string.h>
+#include <dirent.h>
+/* Pour l'utilisation des dossiers */
+
+
+#include <iostream>
+#ifndef WIN32
+    #include <sys/types.h>
+#endif
+
 
 using namespace std;
 
@@ -349,7 +362,7 @@ int Profile::DisplayFasta(string fileName)
 //Reading the protein names of the best hits given by Psi-BLAST
 int Profile::ReadHits(string fileName,vector<string>* hits)
 {
-	string line;
+        string line;
 	ifstream fHandler;
 	fHandler.open((fileName));
 	if (fHandler.is_open())
@@ -433,6 +446,49 @@ int Profile::ReadMSA()
 	ifHandler.close();
 	return 0;
 }
+
+
+
+int Profile::ReadHomstrad()
+{
+    DIR* hom = NULL;
+    struct dirent* metafold = NULL; /* Déclaration d'un pointeur vers la structure dirent. */
+    char chemin[500] = "../2018---2019-partage/Data/HOMSTRAD";
+    hom = opendir(chemin); /* Ouverture du dossier qui contient HOMSTRAD .map */
+
+    if (hom == NULL) /* Si le dossier n'a pas pu être ouvert */
+        exit(1); /* (mauvais chemin par exemple) */
+    puts("Le dossier HOMSTRAD a ete ouvert avec succes");
+
+    while ((metafold = readdir(hom)) != NULL) 
+
+      {
+      printf("Le fichier lu s'appelle '%s'\n", metafold->d_name);
+      DIR* fold_hom = NULL;
+      struct dirent* map = NULL;
+      fold_hom = opendir(strcat(chemin, metafold->d_name));
+      fichier_map=readdir(fold_hom)
+       ifstream myReadFile;
+      myReadFile.open(strcat(metafold->d_name, '.map'));
+      char output[100];
+      if (myReadFile.is_open()) 
+	{
+	while (!myReadFile.eof()) 
+	  {
+          // problem here... 
+
+	  }
+      	}
+      }
+
+
+    if (closedir(hom) == -1) /* S'il y a eu un souci avec la fermeture */
+        exit(-1);
+    puts("Le dossier HOMASTRAD a ete ferme avec succes");
+
+    return 0;
+}
+
 
 int Profile::ProfileName()
 {
