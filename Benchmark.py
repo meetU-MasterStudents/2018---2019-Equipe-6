309dc524279909100b682371510391beae06fd77
Bénédicte Colnet <43812667+BenedicteColnet@users.noreply.github.com>
2018-11-26 11:24:41 +0100

    Benchmark function draft is added
    Work in progress between Amandine and Benedicte

---
+++ Benchmark.py
@@ -0,0 +1,34 @@
+import numpy as np
+import os
+import sys
+
+
+
+### FUNCTIONX ###
+
+
+# Va chercher les .fasta du benchmark
+# Dictionnaire de query + nom
+def RecupereBenchmarkQuery(repertoire):
+    
+    return
+
+
+# Va dans Benchmark.list 
+# output dictionnaire de dictionnaire nom + superfamille et fold associes    
+def RecupereResultBenchmark():
+    
+    return
+
+
+
+# creer dossier avec resultat + documentation
+def main():
+    #nomDossier=sys.argv[1]
+    
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
