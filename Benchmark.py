05acbdfc0b5a45583f8e090a9ef0bfb40e9b8767
Bénédicte Colnet <43812667+BenedicteColnet@users.noreply.github.com>
2018-11-30 00:38:01 +0100

    Benchmark tested for all except the "run"

--- Benchmark.py
+++ Benchmark.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import os
 import sys
 
@@ -7,49 +6,28 @@ import sys
 
 ### FUNCTIONX ###
 
+
 # Va chercher les .fasta du benchmark
 # Dictionnaire de query + nom
-#go into each folder to retrieve each query file.fasta
-
-repertoire='test_dataset'
-
 def RecupereBenchmarkQuery(repertoire):
-    Listqueryfolder=[]
-    Listfasta=[]
-    for nom in os.listdir(repertoire):
-        Listqueryfolder.append(nom)
-        QueryFolder=repertoire+"/"+nom
-        for queryfile in os.listdir(QueryFolder):
-            if queryfile==nom+"."+"fasta": # engros je veux dire si mon extension de fichier est .fasta
-                fastafile=open('test_dataset'+"/"+nom+"/"+nom+"."+"fasta", 'r')
-                fastaread=pd.read_table(fastafile, engine="python")
-                n,m=fastaread.shape
-                Fil=fastaread.iloc[1:n-1,:m-1]
-                fil=Fil.as_matrix()
-                Listfasta.append(fil)
-                fastafile.close()
-    total = {}
-    for x, y in zip(Listqueryfolder, Listfasta):
-        total[x] = y 
-    return Listfasta, Listqueryfolder, total
-
-Data, Name, Dictionnaire_Fasta_Dataset =RecupereBenchmarkQuery(repertoire)
+    
+    return
 
 
 # Va dans Benchmark.list 
 # output dictionnaire de dictionnaire nom + superfamille et fold associes    
-
 def RecupereResultBenchmark():
-    
-    return
+    path = "./Benchmark/Benchmark.list"
+    file = open(path,"r")
+    return file
 
 
 
 # creer dossier avec resultat + documentation
 def main():
     #nomDossier=sys.argv[1]
+    RecupereResultBenchmark()
     
-
 
 if __name__ == "__main__":
     # execute only if run as a script
