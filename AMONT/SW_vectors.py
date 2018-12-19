b126126d6b481f0b10c649410e030073ef35f11a
Bénédicte Colnet <43812667+BenedicteColnet@users.noreply.github.com>
2018-12-19 12:05:34 +0100

    Gap affine extension

--- AMONT/SW_vectors.py
+++ AMONT/SW_vectors.py
@@ -25,7 +25,58 @@ def forward_SW(MatrixN, gp, match):
             scoresSW[i][j] += np.max(possible)
     
     return(scoresSW)
-        
+
+
+
+def local_alignment_affine_gap_penalty(scoring_matrix, gap_opening_penalty, gap_extension_penalty):
+    '''
+    Returns the score and local alignment substrings for strings v, w with the
+    given scoring matrix, gap opening penalty, and gap extension penalty.
+    '''
+    v,w=scoring_matrix.shape
+    # Initialize the matrices.
+    S_lower = [[0 for j in xrange(w+1)] for i in xrange(v+1)]
+    scoresSW = [[0 for j in xrange(w+1)] for i in xrange(v+1)]
+    S_upper = [[0 for j in xrange(w+1)] for i in xrange(v+1)]
+
+    # Initialize the maximum score below the lowest possible score.
+    max_score = -1
+    max_i, max_j = 0, 0
+
+    # Fill in the Score and Backtrack matrices.
+    for i in xrange(1, v+1):
+        for j in xrange(1, w+1):
+            S_lower[i][j] = max([S_lower[i-1][j] - gap_extension_penalty, scoresSW[i-1][j] - gap_opening_penalty])
+            S_upper[i][j] = max([S_upper[i][j-1] - gap_extension_penalty, scoresSW[i][j-1] - gap_opening_penalty])
+            middle_scores = [S_lower[i][j], scoresSW[i-1][j-1] + scoring_matrix[i-1,j-1], S_upper[i][j], 0]
+            scoresSW[i][j] = max(middle_scores)
+
+            if scoresSW[i][j] > max_score:
+                max_score = scoresSW[i][j]
+                max_i, max_j = i, j
+
+#    # Initialize the indices to start at the position of the high score.
+#    i, j = max_i, max_j
+#
+#    # Initialize the aligned strings as the input strings up to the position of the high score.
+#    v_aligned, w_aligned = v[:i], w[:j]
+#
+#    # Backtrack to start of the local alignment starting at the highest scoring cell.
+#    # Note: the solution format specifically asks for substrings, so no indel insertion necessary.
+#    while backtrack[i][j] != 3 and i*j != 0:
+#        if backtrack[i][j] == 0:
+#            i -= 1
+#        elif backtrack[i][j] == 1:
+#            i -= 1
+#            j -= 1
+#        elif backtrack[i][j] == 2:
+#            j -= 1
+#
+#    # Cut the strings at the ending point of the backtrack.
+#    v_aligned = v_aligned[i:]
+#    w_aligned = w_aligned[j:]
+
+    return(scoresSW)  
 
 
 
@@ -71,3 +122,8 @@ def traceback_SW(ScoresSW, give_alignment = True):
         return(np.max(ScoresSW),ngap, start_query, start_template, end_query, end_template, sorted(align_seq1), sorted(align_seq2))
     else:
         return(traceback)
+
+
+
+
+
